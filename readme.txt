ba3a90665d9ddfe45fae3f9399822a343ce3ace4
Masoud Melerdi <Masoud Melerdi@localhost>
2012-11-29 16:10:40 +0800

    windowStateChanging package and its sub states have been created.

---
+++ readme.txt
@@ -0,0 +1,2 @@
+
+add(comp) the component should not be a window.java exception error(adding a window to a container)
---
+++ src/plaidgui/auxiliary/WindowListener.java
@@ -0,0 +1,26 @@
+package plaidgui.auxiliary;
+
+import java.awt.event.*;
+import plaid.runtime.*;
+
+// TODO: replace this with pure Plaid code when Plaid/Java interop improves
+public class WindowListener extends WindowAdapter {
+	PlaidMethod plaidFunction;
+	public WindowListener(PlaidMethod f) {
+		plaidFunction = f;
+	}
+	
+	@Override
+	
+	public void windowIconified(WindowEvent e){
+		 System.out.println("Window Iconofied");
+			plaidFunction.invoke(Util.unit());
+	 }
+	public void windowClosing(WindowEvent e) {
+		System.out.println("invoking close");
+		plaidFunction.invoke(Util.unit());
+	}
+	
+	 
+	
+}
